902755b6015e6bdd1ee3257ac307babeb953e69e
ReR-7 <fernando_1320114003@uptecamac.edu.mx>
2023-05-04 23:49:03 -0600

    Agregar archivos de proyecto.

---
+++ FiltroImagenesGrises/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace FiltroImagenesGrises
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbOriginal = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pbGris = new System.Windows.Forms.PictureBox();
+            this.pbBinario = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.insertarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirImagenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grisesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.binarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGris)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBinario)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbOriginal
+            // 
+            this.pbOriginal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbOriginal.Location = new System.Drawing.Point(39, 97);
+            this.pbOriginal.Name = "pbOriginal";
+            this.pbOriginal.Size = new System.Drawing.Size(250, 250);
+            this.pbOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOriginal.TabIndex = 0;
+            this.pbOriginal.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pbGris
+            // 
+            this.pbGris.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbGris.Location = new System.Drawing.Point(320, 97);
+            this.pbGris.Name = "pbGris";
+            this.pbGris.Size = new System.Drawing.Size(250, 250);
+            this.pbGris.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGris.TabIndex = 1;
+            this.pbGris.TabStop = false;
+            // 
+            // pbBinario
+            // 
+            this.pbBinario.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbBinario.Location = new System.Drawing.Point(597, 97);
+            this.pbBinario.Name = "pbBinario";
+            this.pbBinario.Size = new System.Drawing.Size(250, 250);
+            this.pbBinario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBinario.TabIndex = 2;
+            this.pbBinario.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insertarToolStripMenuItem,
+            this.herramientasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(902, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // insertarToolStripMenuItem
+            // 
+            this.insertarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirImagenToolStripMenuItem});
+            this.insertarToolStripMenuItem.Name = "insertarToolStripMenuItem";
+            this.insertarToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.insertarToolStripMenuItem.Text = "Insertar";
+            // 
+            // abrirImagenToolStripMenuItem
+            // 
+            this.abrirImagenToolStripMenuItem.Name = "abrirImagenToolStripMenuItem";
+            this.abrirImagenToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.abrirImagenToolStripMenuItem.Text = "Abrir Imagen";
+            this.abrirImagenToolStripMenuItem.Click += new System.EventHandler(this.abrirImagenToolStripMenuItem_Click);
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grisesToolStripMenuItem,
+            this.binarioToolStripMenuItem});
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.herramientasToolStripMenuItem.Text = "Herramientas";
+            // 
+            // grisesToolStripMenuItem
+            // 
+            this.grisesToolStripMenuItem.Name = "grisesToolStripMenuItem";
+            this.grisesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.grisesToolStripMenuItem.Text = "Grises";
+            this.grisesToolStripMenuItem.Click += new System.EventHandler(this.grisesToolStripMenuItem_Click);
+            // 
+            // binarioToolStripMenuItem
+            // 
+            this.binarioToolStripMenuItem.Name = "binarioToolStripMenuItem";
+            this.binarioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.binarioToolStripMenuItem.Text = "Binario";
+            this.binarioToolStripMenuItem.Click += new System.EventHandler(this.binarioToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 450);
+            this.Controls.Add(this.pbBinario);
+            this.Controls.Add(this.pbGris);
+            this.Controls.Add(this.pbOriginal);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGris)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBinario)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbOriginal;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pbGris;
+        private System.Windows.Forms.PictureBox pbBinario;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem insertarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirImagenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grisesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem binarioToolStripMenuItem;
+    }
+}
+
---
+++ FiltroImagenesGrises/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiltroImagenesGrises
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirImagenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog()
+            {
+                Filter = "Archivos de Imagen (*.jpg)|*.jpg|All files (*.*)|*.*"
+            };
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pbOriginal.Image = Image.FromFile(open.FileName);
+            }
+        }
+
+        private void grisesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap imagen = new Bitmap(pbOriginal.Image);
+            Bitmap imagenGrises = new Bitmap(imagen.Width, imagen.Height);
+
+            for(int x = 0; x < imagen.Width; x++)
+            {
+                for (int Y = 0; Y < imagen.Height; Y++)
+                {
+                    Color pixel = imagen.GetPixel(x, Y);
+                    Byte pixelGris= (byte)(pixel.R * 0.3 + pixel.G * 0.59 + pixel.B * 0.11);
+
+                    imagenGrises.SetPixel(x,Y,Color.FromArgb(pixelGris, pixelGris, pixelGris));
+                }
+            }
+            pbGris.Image = imagenGrises;
+        }
+
+        private void binarioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap imagen = new Bitmap(pbOriginal.Image);
+            Bitmap imagenBinario = new Bitmap(imagen.Width, imagen.Height);
+
+            for (int x = 0; x < imagen.Width; x++)
+            {
+                for (int Y = 0; Y < imagen.Height; Y++)
+                {
+                    Color pixel = imagen.GetPixel(x, Y);
+                 
+                    if (pixel.R >= 128)
+                    {
+                        imagenBinario.SetPixel(x, Y, Color.FromArgb(255, 255, 255));
+                    }
+                    else
+                    {
+                        imagenBinario.SetPixel(x, Y, Color.FromArgb(0, 0, 0));
+                    }
+                }
+            }
+            pbBinario.Image = imagenBinario;
+        }
+    }
+}
