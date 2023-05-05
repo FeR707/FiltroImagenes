e89cab56cc3d3e99f26a57fd0f773aa461a5ddf3
ReR-7 <fernando_1320114003@uptecamac.edu.mx>
2023-05-05 00:04:49 -0600

    Coment

--- FiltroImagenesGrises/Form1.cs
+++ FiltroImagenesGrises/Form1.cs
@@ -19,11 +19,14 @@ namespace FiltroImagenesGrises
 
         private void abrirImagenToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            // Abrir un archivo de imagen
             OpenFileDialog open = new OpenFileDialog()
             {
+                // Filtro para archivos de imagen
                 Filter = "Archivos de Imagen (*.jpg)|*.jpg|All files (*.*)|*.*"
             };
 
+            // Si se selecciona un archivo de imagen
             if (open.ShowDialog() == DialogResult.OK)
             {
                 pbOriginal.Image = Image.FromFile(open.FileName);
@@ -35,13 +38,16 @@ namespace FiltroImagenesGrises
             Bitmap imagen = new Bitmap(pbOriginal.Image);
             Bitmap imagenGrises = new Bitmap(imagen.Width, imagen.Height);
 
+            // Recorrer la imagen
             for(int x = 0; x < imagen.Width; x++)
             {
                 for (int Y = 0; Y < imagen.Height; Y++)
                 {
+                    // Obtener el pixel de la imagen original
                     Color pixel = imagen.GetPixel(x, Y);
+                    // Calcular el promedio de los colores
                     Byte pixelGris= (byte)(pixel.R * 0.3 + pixel.G * 0.59 + pixel.B * 0.11);
-
+                    // Crear el nuevo pixel en la imagen en escala de grises
                     imagenGrises.SetPixel(x,Y,Color.FromArgb(pixelGris, pixelGris, pixelGris));
                 }
             }
@@ -53,18 +59,22 @@ namespace FiltroImagenesGrises
             Bitmap imagen = new Bitmap(pbOriginal.Image);
             Bitmap imagenBinario = new Bitmap(imagen.Width, imagen.Height);
 
+            // Recorrer la imagen
             for (int x = 0; x < imagen.Width; x++)
             {
                 for (int Y = 0; Y < imagen.Height; Y++)
                 {
+                    // Obtener el pixel de la imagen original
                     Color pixel = imagen.GetPixel(x, Y);
-                 
+                    // Comparar el valor del pixel si es mayor a 128
                     if (pixel.R >= 128)
                     {
+                        // Si es mayor a 128 se pone el pixel en blanco
                         imagenBinario.SetPixel(x, Y, Color.FromArgb(255, 255, 255));
                     }
                     else
                     {
+                        // Si es menor se pone el pixel en negro
                         imagenBinario.SetPixel(x, Y, Color.FromArgb(0, 0, 0));
                     }
                 }
